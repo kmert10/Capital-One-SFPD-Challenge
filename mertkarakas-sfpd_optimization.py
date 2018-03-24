0175af4bee18a0edc62be920abbc2d796572ab0a
Mert Karakas <mertkarakas1997@gmail.com>
2018-03-23 22:10:53 -0400

    Additions
    Added 2 new map graphs for question 1.   Also added code for stacked bar graph for zip code - unit type correlation

--- mertkarakas-sfpd_optimization.py
+++ mertkarakas-sfpd_optimization.py
@@ -5,10 +5,13 @@ Capital One Challenge: Can you optimize and predict emergency calls?
 Mert Karakas
 UVA 2020
 """
+
 import pandas as pd
 import numpy as np
 from  matplotlib import pyplot
 import seaborn
+import plotly.plotly as py
+import plotly.graph_objs as go
 seaborn.set(style='ticks')
 
 
@@ -26,6 +29,39 @@ hour = pd.Series(hours)
 d = {'Time (Hour)':hour, 'Zip Codes':data['zipcode_of_incident'], 'unit_type':data['unit_type']}
 ut = pd.DataFrame(data=d)
 units = np.unique(ut['unit_type'])
-fg = seaborn.FacetGrid(data=ut[0:500], hue='unit_type', hue_order=units,size=10, aspect=1.61)
+fg = seaborn.FacetGrid(data=ut[0:2000], hue='unit_type', hue_order=units,size=10, aspect=1.61)
 fg.map(pyplot.scatter, 'Time (Hour)', 'Zip Codes').add_legend()
+
+days = []
+for time in range(len(data['call_date'])):
+    days.append(int(data['call_date'].loc[time].split('-')[2]))
+day = pd.Series(days)
+
+d = {'Days in January':day, 'Zip Codes':data['zipcode_of_incident'], 'unit_type':data['unit_type']}
+ut = pd.DataFrame(data=d)
+x = np.linspace(1, 10, len(ut['Zip Codes']))
+dt = [
+    go.Bar(
+        x=ut['Zip Codes'], # assign x as the dataframe column 'x'
+        y=ut['unit_type']
+    ),
+    go.Bar(
+        x=ut['Zip Codes'], # assign x as the dataframe column 'x',
+        y=ut['unit_type']
+    ),
+    go.Bar(
+        x=ut['Zip Codes'], # assign x as the dataframe column 'x',
+        y=ut['unit_type']
+    ),
+    go.Bar(
+        x=ut['Zip Codes'], # assign x as the dataframe column 'x',
+        y=ut['unit_type']
+    )
+]
+layout = go.Layout(
+    barmode='stack',
+    title='Stacked Bar with Pandas'
+)
+
+fig = go.Figure(data=data, layout=layout)
 # 3. Which areas take the longest time to dispatch to on average? How can this be reduced?
