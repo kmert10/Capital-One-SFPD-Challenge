046643731502716c4e2639f103acce77f0c1b6e7
Mert Karakas <mertkarakas1997@gmail.com>
2018-03-23 21:31:52 -0400

    Scatter plot
    Added 1 Scatter plot for question 2

--- mertkarakas-sfpd_optimization.py
+++ mertkarakas-sfpd_optimization.py
@@ -7,6 +7,9 @@ UVA 2020
 """
 import pandas as pd
 import numpy as np
+from  matplotlib import pyplot
+import seaborn
+seaborn.set(style='ticks')
 
 
 data = pd.read_csv('sfpd_dispatch_data_subset.csv') # read in .csv into a pd.Dataframe
@@ -16,6 +19,13 @@ schema = pd.read_csv('sfpd_dispatch_schema.csv') # read in .csv into a pd.Datafr
 
 
 # 2. Given an address and time, what is the most likely dispatch to be required?
-ut = pd.concat([data['unit_type'], data['']
-
+hours = []
+for time in range(len(data['dispatch_timestamp'])):
+    hours.append(int(data['dispatch_timestamp'].loc[time].split()[1].split(':')[0]))
+hour = pd.Series(hours)
+d = {'Time (Hour)':hour, 'Zip Codes':data['zipcode_of_incident'], 'unit_type':data['unit_type']}
+ut = pd.DataFrame(data=d)
+units = np.unique(ut['unit_type'])
+fg = seaborn.FacetGrid(data=ut[0:500], hue='unit_type', hue_order=units,size=10, aspect=1.61)
+fg.map(pyplot.scatter, 'Time (Hour)', 'Zip Codes').add_legend()
 # 3. Which areas take the longest time to dispatch to on average? How can this be reduced?
